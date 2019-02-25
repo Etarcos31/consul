e5b568fbdc80eaf5a66b1c8caddefbc0f39e9fd9
Etarcos31 <remi.bensoussan@epl-lad.org>
2019-02-25 21:51:14 +0100

    Update application_custom.rb

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,6 @@
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = :en
-    config.i18n.available_locales = [:en, :es]
+    config.i18n.default_locale = :fr
+    config.i18n.available_locales = [:fr, :en]
   end
 end
